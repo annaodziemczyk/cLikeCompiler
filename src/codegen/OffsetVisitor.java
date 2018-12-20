b7fa7076ea52dd49d557016685396fd6d77caad6
annaodziemczyk <anna.odziemczyk@mycit.ie>
2018-12-20 22:37:02 +0000

    offset visitor

--- src/codegen/OffsetVisitor.java
+++ src/codegen/OffsetVisitor.java
@@ -1,17 +1,42 @@
 package codegen;
 
+import ast.FunctionDefinition;
 import ast.Record;
+import ast.Statement;
 import ast.TypeDefinition;
 import ast.VarDefinition;
 import visitor.AbstractVisitor;
 
 public class OffsetVisitor extends AbstractVisitor<Void, Void> {
 private int bytesGlobalsSum = 0;
+private int bytesLocalSum = 0;
 	
 	@Override
 	public Void visit(VarDefinition varDefinition, Void param) {
-		varDefinition.setOffset(bytesGlobalsSum);
-		bytesGlobalsSum += varDefinition.getType().numberOfBytes();
+		if(varDefinition.getScope()==0) {
+			varDefinition.setOffset(bytesGlobalsSum);
+			bytesGlobalsSum += varDefinition.getType().numberOfBytes();
+		}else {
+			varDefinition.setOffset(bytesLocalSum);
+			bytesLocalSum += varDefinition.getType().numberOfBytes();
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public Void visit(FunctionDefinition functionDefinition, Void param) {
+		functionDefinition.setOffset(bytesGlobalsSum);
+		for(VarDefinition varDef:functionDefinition.getVariables()) {
+			varDef.accept(this, param);
+		}
+		
+		for(Statement statement:functionDefinition.getFunctionBody()) {
+			statement.accept(this, param);
+		}
+		
+		bytesLocalSum=0;
+		
 		return null;
 	}
 	
